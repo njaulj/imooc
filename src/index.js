64ca25891897d7cf197120067f89457e98de9906
Stern <stern_zhang@wochacha.com>
2014-09-25 11:18:24 +0800

    get seperate chapter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ var clToken = '7f75e24cb1f7e5c358f03a7b40a60976',
 		uid: 0,
 		page: 1,
 		token: clToken
-	}, 
+	},
 	ciParams = {
 		uid: 0,
 		cid: '',
@@ -20,10 +20,11 @@ var clToken = '7f75e24cb1f7e5c358f03a7b40a60976',
 var iMooc = function() {
 };
 
-iMooc.prototype.list = function(page) {
+iMooc.prototype.list = function(page, keyword) {
 	clParams.page = page;
+	clParams.keyword = keyword || '';
 	var options = { url: courselistURL, form: clParams };
-	this.post(options, function(err, resJSON) {
+	this._post(options, function(err, resJSON) {
 		if (!err) {
 			var courses = resJSON.data;
 			for (var i = 0, len = courses.length; i < len; i++) {
@@ -38,7 +39,7 @@ iMooc.prototype.list = function(page) {
 iMooc.prototype.show = function(cid) {
 	ciParams.cid = cid;
 	var options = {url: courseinfoURL, form: ciParams};
-	this.post(options, function(err, response) {
+	this._post(options, function(err, response) {
 		if (!err) {
 			var chapters = response.data;
 			for (var i = 0, len = chapters.length; i < len; i++) {
@@ -52,10 +53,29 @@ iMooc.prototype.show = function(cid) {
 
 iMooc.prototype.download = function(cid, chapterid) {
 	ciParams.cid = cid;
-	this.post();
+	var options = {url: courseinfoURL, form: ciParams};
+	this._post(options, function(err, response) {
+		if (!err) {
+			var course = response.data,
+				len = course.length;
+			if (chapterid < len) {
+				var chapter = course[chapterid - 1],
+					chapterInfo = chapter.chapter,
+					medias = chapter.media;
+				for (var i = 0, len = medias.length; i < len; i++) {
+					console.log(chapterInfo.name);
+					console.log(medias[i].name, medias[i].media_url);
+				}
+			} else {
+
+			}
+		} else {
+
+		}
+	});
 };
 
-iMooc.prototype.post = function (options, callback) {
+iMooc.prototype._post = function (options, callback) {
 	request.post(options, function(err, response, body) {
 		if (!err && response.statusCode == 200) {
 			var data = JSON.parse(body);
