fe14bd0b97ea355f67f98b62be34a21e090342e3
stern <stern_zhang@wochacha.com>
2014-09-26 22:08:14 +0800

    work on 0.0.2

--- src/index.js
+++ src/index.js
@@ -43,9 +43,106 @@ exports.list = function(page, cb) {
 	imoocPost(listOptions, cb);
 }
 
+exports.get = function(cid, chapters, destdir, cb) {
+	coParams.cid = cid;
+	var getOptions = { url: courseintroURL, form: coParams};
+	async.waterfall([
+		function(next) {
+			imoocPost(getOptions, function(err, data) {
+				if (err) return next(err);
+				next(null, data);
+			});
+		},
+		function(data, next) {
+			var content = ['课程名称：' + data[0].course_name,
+				'课程简介：' + data[0].course_des].join(os.EOL);
+			fs.writeFile(path.join(destdir, 'README.txt'), content, function(err) {
+				next(err);
+			});
+		},
+		function(next) {
+			ciParams.cid = cid;
+			var cinOptions = {url: courseinfoURL, form: ciParams};
+			imoocPost(cinOptions, function(err, data) {
+				if (err) {
+					return next(err);
+				}
+				var len = data.length,
+					seqs = [];
+				// 获取课程章节
+				data.forEach(function(c) {
+					seqs.push(c.chapter.seq);
+				});
+				chapters = chapters.length ? chapters : seqs;
+				next(null, chapters, seqs, data);
+			});
+		},
+		function(chapters, seqs, data, next) {
+			async.eachLimit(chapters, 5, function(id, callback) {
+				if (seqs.indexOf(id) !== -1) {
+					var chapter = data[id - 1];
+					imoocDownload(chapter, destdir, callback);
+				} else {
+					callback();
+				}
+			}, function(err) {
+				next(err);
+			});
+		}
+	], function(err) {
+		// if (err) ;
+		cb(err);
+	});
+}
 
 exports.error = function(error) {
 	console.log('错误'.red + ' ' + (error instanceof Object ? error.message : error));
+}
+
+function imoocDownload(course, destdir, callback) {
+	var chapter = course.chapter,
+		media = course.media,
+		chapdir = path.join(destdir, chapter.seq + '_' + chapter.name);
+	async.waterfall([
+		function(next) {
+			fs.exists(chapdir, function(exists) {
+				next(null, exists);
+			});
+		},
+		function(exists, next) {
+			if (exists) return next(null);
+			mkdirp(chapdir, function(err) {
+				next(err);
+			});
+		},
+		function(next) {
+			async.each(media, function(m, next) {
+				var url = m.media_url,
+					name = m.name,
+					seq = m.media_seq,
+					ext = path.extname(url),
+					base = path.basename(url, ext),
+					dir = path.dirname(url),
+					savepath = path.join(chapdir, seq+'_'+name+ext),
+					vstream = null;
+				if (base == 'L' && /mp4/.test(ext)) {
+					url = dir + '/H' + ext;
+				}
+				vstream = fs.createWriteStream(savepath);
+				request.get(url).on('data', function(data) {
+					vstream.write(data);
+				}).on('end', function() {
+					next();
+				}).on('error', function(err) {
+					next();
+				});
+			}, function(err) {
+				next(err);
+			});
+		}
+	], function(err) {
+		callback(err);
+	});
 }
 
 function imoocPost(options, callback) {
